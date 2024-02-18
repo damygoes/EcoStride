81aba4efdd43b8c61f3747ea263b41e4c768ff86
Damilola Bada <damilola.bada@apomap.de>
2024-02-18 14:56:04 +0100

    chore(use-login.tsx): removed setAuthToken function as it is no longer needed

--- src/utils/auth/use-login.ts
+++ src/utils/auth/use-login.ts
@@ -1,11 +1,10 @@
 import { CredentialResponse } from "@react-oauth/google";
 import { axiosClient } from "@services/axios/axios-client";
-import { useUser, useUserStore } from "@utils/user/user-store";
+import { useUser } from "@utils/user/user-store";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
   const { setUser } = useUser();
-  const { setAuthToken } = useUserStore();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
@@ -16,13 +15,10 @@ export const useLogin = () => {
       const response = await axiosClient.post("/auth/google", {
         token: googleToken,
       });
-      // const response = await axiosClient.post("/api/auth", {
-      //   token: googleToken,
-      // });
+
       const user = response.data;
       const userDoc = user?.user;
       setUser({ ...userDoc });
-      setAuthToken(user.token);
       navigate(from);
     } catch (error) {
       console.error("Error creating user document", error);
